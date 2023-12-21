33c6a42e7229f447b617203d64391004185abf02
Agostina Lamboglia <agoslamboglia@gmail.com>
2023-12-21 18:28:46 -0300

    axios - importando api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
+import { useEffect, useState } from "react";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getProducts } from "./services";
 
 const App = () => {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    getProducts()
+      .then((response) => {
+        setProductsData(response.data.products)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [])
+
   return(
-    <div style={{width:'100vh', height: '100vh'}}>
+    <div style={{width:'100vw', height: '100vh'}}>
       <><NavBar></NavBar></>
-      <><ItemListContainer greeting="Hola mundo"></ItemListContainer></>
-      
+      <><ItemListContainer greeting="Agostina Shop" productsData={productsData} ></ItemListContainer></>
     </div>
   );
   
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,5 @@
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = ({greeting, productsData}) => {
+    console.log(productsData);
     return(
         <div style={{fontWeight: 'bold', fontSize:'2rem', width:'100vw', height:'50vh', textAlign:'center'}}>
             {greeting}
